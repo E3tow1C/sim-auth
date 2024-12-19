886c0b3ba797748d05f861afbcbf1f14447238a6
Thepphithak <95979163+E3tow1C@users.noreply.github.com>
2024-12-19 16:38:07 +0700

    add validations for article header and body presence with length constraint

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  validates :header, presence: { message: "Header can't be blank" }
+  validates :body, presence: { message: "Body can't be blank" }, length: { maximum: 100, message: "Max length 100 characters" }
 end
