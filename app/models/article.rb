c1e70f533f3f2b56f8f44b9cf9a9cf7ee52775e9
Thepphithak <95979163+E3tow1C@users.noreply.github.com>
2024-12-19 17:13:30 +0700

    add author validation to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   validates :header, presence: { message: "ชื่อเรื่องต้องไม่ว่างเปล่า" }
   validates :body, presence: { message: "เนื้อหาต้องไม่ว่างเปล่า" }, length: { maximum: 100, message: "เนื้อหาต้องมีความยาวไม่เกิน 100 ตัวอักษร" }
+  validates :author, presence: { message: "ผู้เขียนต้องไม่ว่างเปล่า" }, length: { maximum: 100, message: "ชื่อผู้เขียนต้องมีความยาวไม่เกิน 100 ตัวอักษร" }
 end
