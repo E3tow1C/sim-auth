539484e6d9899b47761de8534925187b8cbb08ce
Thepphithak <95979163+E3tow1C@users.noreply.github.com>
2024-12-19 18:25:00 +0700

    refactor article form submit button text logic

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ApplicationRecord
   validates :header, presence: { message: "ชื่อเรื่องต้องไม่ว่างเปล่า" }
   validates :body, presence: { message: "เนื้อหาต้องไม่ว่างเปล่า" }, length: { maximum: 100, message: "เนื้อหาต้องมีความยาวไม่เกิน 100 ตัวอักษร" }
   validates :author, presence: { message: "ผู้เขียนต้องไม่ว่างเปล่า" }, length: { maximum: 100, message: "ชื่อผู้เขียนต้องมีความยาวไม่เกิน 100 ตัวอักษร" }
+
+  def save_button_article
+    new_record? ? "สร้างบทความใหม่" : "แก้ไขบทความ"
+  end
 end
